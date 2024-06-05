0644a29b02746d06f0bbce1e2a81112eea55702a
Jason <jasonbalderrama6@gmail.com>
2024-06-05 14:45:42 -0400

    comments

--- Workbook7/exercises/exercise-1/scripts/index.js
+++ Workbook7/exercises/exercise-1/scripts/index.js
@@ -1,4 +1,32 @@
-"use strict"
-window.onload = function () {
+"use strict";
+
+// HTML element variables
+const idInput = document.querySelector("#idInput");
+const displayButton = document.querySelector("#displayButton");
+const infoParagraph = document.querySelector("#infoParagraph");
+//functions
+
+function displayTodo(params) {
     
+//  user puts ID number in idInput
+
+
+// user clicks displayButton
+
+// computer recieves idInput value
+
+// computer builds the url which is http://jsonplaceholder.typicode.com/users/ plus the input id value 
+
+// computer calls api using url provided
+
+// computer display todo info 
+
 }
+
+
+//event handling
+displayButton.onclick = displayTodo;
+
+
+
+//initial loading
